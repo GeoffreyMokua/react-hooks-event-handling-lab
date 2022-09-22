55de964f47be7ee11058c486bf3e531d29d27a92
GeoffreyMokua <geoffrey.nyakundi@student.moringaschool.com>
2022-09-22 15:24:54 +0300

    completed assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,14 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+const EyesOnMe = () => {
+  return (
+    <div>
+        <form>
+            <button type='submit' onBlur={event => console.log('Hey! Eyes on me!')}  onFocus={event => console.log('Good!')}> Eyes on me</button>
+        </form>
+    </div>
+  )
+}
+
+export default EyesOnMe
